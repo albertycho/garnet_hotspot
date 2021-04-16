6856089fd525093dc6705e154776c3e49156e688
akittel3 <akittel3@gatech.edu>
2021-04-15 22:00:38 -0400

    changed visualization window index to a sliding window instead

--- hotspot_visualizer_mesh.py
+++ hotspot_visualizer_mesh.py
@@ -2,6 +2,8 @@ import cv2 as cv
 import numpy as np
 from parse_data import parseData
 
+num_ports = 4 # 2D mesh, 4 ports - excluding local port
+
 def create_heat_maps(cycle_data, topology_info):
     """
     Parses the cycle data read from a .csv file into labeling of number of flits
@@ -16,7 +18,6 @@ def create_heat_maps(cycle_data, topology_info):
                    For mesh, there can be at most 5 each cycle (4 links + local port)
     """
 
-    num_ports = 4 # assume 4 ports for now in the 2D mesh (exclude local port)
     port_directions = ["South", "East", "North", "West"]
     sim_cycles = cycle_data[-1]["cycle"]
     routers = cycle_data[cycle_data["unit"] == "InUnit"]
@@ -38,26 +39,8 @@ def create_heat_maps(cycle_data, topology_info):
             heat_map_ports[dir_cycles,(i*num_ports)+j] = 1
     return heat_map_routers, heat_map_ports
 
-def condense_heat_map(heat_map, time_window):
-    """
-    Condenses the heat map produced by create_heat_map from sim_cycles rows down to
-    sim_cycles//time_window rows, averaging the values across each window.
-
-    Average values are then truncated down to be in range [0-1] for display in draw_mesh.
-
-    Inputs:
-        heat_map - heat map produced by create_heat_map
-        time_window - integer for number of cycles to average the heat map over.
-                      Higher time_windows will produce a condensed heat map with fewer rows.
-    Outputs:
-        condense_heat_map - condensed heat map, with values truncated between 0 and 1 for display
-    """
-
-    num_windows = heat_map.shape[0]//time_window
-    condense_heat_map = np.zeros((num_windows, heat_map.shape[1]))
-    for i in range(num_windows):
-        condense_heat_map[i,:] = np.sum(heat_map[i*time_window:i*time_window+time_window,:], axis=0)/time_window
-    return condense_heat_map
+def heat_map_window(heat_map, time_window, window_offset):
+    return np.sum(heat_map[window_offset:window_offset+time_window,:], axis=0) / time_window
 
 def draw_mesh(heat_map, topology_info, n, router_display):
     """ Creates an image of a mesh network, with routers color coded.
@@ -70,7 +53,6 @@ def draw_mesh(heat_map, topology_info, n, router_display):
             img - ~1000x1000 image with the drawn mesh topology, to be displayed in the openCV GUI
     """
 
-    num_ports = 4
     img_size = 1000 # set img size as 1000x1000
     num_rows = topology_info[1]
     router_width = img_size//2//num_rows # pixel width for a drawn router
@@ -150,44 +132,49 @@ def main():
     # change these for the current .csv file
     filename = r'traceFiles\XY_Mesh_4x4_UniformRandom_50.csv'
     topology = "mesh"
-    window_size = 100
+
+    #TODO don't hardcode sim_cycles, output to .csv file in garnet
     sim_cycles = 10000
+
+    # initial window size and offset for display
+    window_size = 1000
+    window_offset = 1000
 
     # parse the .csv file
     cycle_data, _, topology_info = parseData(filename, topology)
 
     # compute an initial heat map
     heat_map_routers, heat_map_ports = create_heat_maps(cycle_data, topology_info)
-    heat_map_routers_condense = condense_heat_map(heat_map_routers, window_size) / 4.0
-    heat_map_ports_condense = condense_heat_map(heat_map_ports, window_size)
+    heat_map_routers_window = heat_map_window(heat_map_routers, window_size, window_offset) / num_ports
+    heat_map_ports_window = heat_map_window(heat_map_ports, window_size, window_offset)
 
     # create the GUI window and trackbars
     cv.namedWindow('Heatmap', cv.WINDOW_NORMAL)
-    cv.resizeWindow('Heatmap',1000,1000) # default size is 1000x1000px, but it is resizable
-    cv.createTrackbar('Window Size','Heatmap',window_size-1,int(sim_cycles//10-1),trackbar_nothing)
-    cv.createTrackbar('Window Index','Heatmap',0,int((sim_cycles//10-1)//10),trackbar_nothing)
-    cv.createTrackbar('Most Active Routers','Heatmap',0,topology_info[1]**2,trackbar_nothing)
-    cv.createTrackbar('Toggle Router/Port View','Heatmap',0,1,trackbar_nothing)
+    cv.resizeWindow('Heatmap', 1000, 1000) # default size is 1000x1000px, but it is resizable
+    cv.createTrackbar('Window Size', 'Heatmap', window_size-1, sim_cycles-1, trackbar_nothing)
+    cv.createTrackbar('Window Offset', 'Heatmap', window_offset, sim_cycles-1, trackbar_nothing)
+    cv.createTrackbar('Most Active Routers', 'Heatmap', 0, topology_info[1]**2, trackbar_nothing)
+    cv.createTrackbar('Toggle Router/Port View', 'Heatmap', 0, 1, trackbar_nothing)
 
     # set tracked variables to their initial values for the first pass through below loop
-    window_index = cv.getTrackbarPos('Window Index', 'Heatmap')
+    old_window_offset = cv.getTrackbarPos('Window Offset', 'Heatmap')
     old_window_size = cv.getTrackbarPos('Window Size', 'Heatmap') + 1
     most_active = cv.getTrackbarPos('Most Active Routers', 'Heatmap')
     router_display = cv.getTrackbarPos('Toggle Router/Port View','Heatmap')
 
     while(1):
-        # draw the mesh
+        # draw the mesh, either with routers color coded, or the ports
         if router_display == 0:
-            cv.imshow('Heatmap', draw_mesh(heat_map_routers_condense[window_index,:], topology_info, most_active, router_display))
+            cv.imshow('Heatmap', draw_mesh(heat_map_routers_window, topology_info, most_active, router_display))
         else:
-            cv.imshow('Heatmap', draw_mesh(heat_map_ports_condense[window_index,:], topology_info, most_active, router_display))
+            cv.imshow('Heatmap', draw_mesh(heat_map_ports_window, topology_info, most_active, router_display))
 
         k = cv.waitKey(1) & 0xFF # wait for 1ms
         if k == 27: # hit escape to end the program
             break
 
         # get current positions of trackbars
-        window_index = cv.getTrackbarPos('Window Index', 'Heatmap')
+        window_offset = cv.getTrackbarPos('Window Offset', 'Heatmap')
         window_size = cv.getTrackbarPos('Window Size', 'Heatmap') + 1
         most_active = cv.getTrackbarPos('Most Active Routers', 'Heatmap')
         router_display = cv.getTrackbarPos('Toggle Router/Port View','Heatmap')
@@ -195,16 +182,17 @@ def main():
         # no way I could find to change the bound of a trackbar once you create it.
         # manually set the position to the maximum possible window_indedx given
         # the current window_size if the user slides it over the maximum.
-        if window_index > int(sim_cycles/window_size-1):
-            window_index = int(sim_cycles/window_size-1)
-            cv.setTrackbarPos('Window Index', 'Heatmap', window_index)
+        if window_offset > sim_cycles - window_size:
+            window_offset = sim_cycles - window_size
+            cv.setTrackbarPos('Window Offset', 'Heatmap', window_offset)
 
         # the user changes the window_size, so need to recompute the heat map.
-        if window_size != old_window_size:
-            heat_map_routers_condense = condense_heat_map(heat_map_routers, window_size) / 4.0
-            heat_map_ports_condense = condense_heat_map(heat_map_ports, window_size)
+        if window_size != old_window_size or window_offset != old_window_offset:
+            heat_map_routers_window = heat_map_window(heat_map_routers, window_size, window_offset) / num_ports
+            heat_map_ports_window = heat_map_window(heat_map_ports, window_size, window_offset)
 
         old_window_size = window_size
+        old_window_offset = window_offset
 
     cv.destroyAllWindows()
 
